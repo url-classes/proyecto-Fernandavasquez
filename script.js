d2147ed89b05726a906f85bd9fb68178b928b47e
FernandaVasquez <castilloferlisbeh@gmail.com>
2024-05-21 22:56:46 -0600

    Avances

--- script.js
+++ script.js
@@ -15,3 +15,24 @@ function seleccionar(){
     document.getElementById("nav").classList = "";
     menuVisible = false;
 }
+
+
+//Funcion para las animaciones de mis skilss
+function efectoHabilidades(){
+    var skills = document.getElementById("skills");
+    var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
+    if(distancia_skills >= 300){
+        let habilidades = document.getElementsByClassName("progreso");
+        habilidades[0].classList.add("python");
+        habilidades[1].classList.add("javacript");
+        habilidades[2].classList.add("cS");
+        habilidades[5].classList.add("comunicacion");
+        habilidades[6].classList.add("trabajo");
+        habilidades[7].classList.add("creatividad");
+        habilidades[8].classList.add("dedicacion");
+        habilidades[9].classList.add("liderazgo");
+        habilidades[10].classList.add("responsable");
+        habilidades[11].classList.add("compromiso");
+
+    }
+}
